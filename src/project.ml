115a3b678e54e83724f147bff90562fdef5d4f0d
Richie Sun <rs929@cornell.edu>
2023-04-25 13:23:12 -0400

    Implemented Block Creation

--- src/project.ml
+++ src/project.ml
@@ -21,31 +21,45 @@ let stay_rect2 = Raylib.Rectangle.create 10. 100. 100. 40.
 let turn_rect = Raylib.Rectangle.create 10. 150. 100. 40.
 let stay_rect = Raylib.Rectangle.create 10. 150. 100. 40.
 let start_button = Raylib.Rectangle.create 10. 200. 100. 40.
+let stay_rect3 = Raylib.Rectangle.create 10. 200. 100. 40.
 let end_button = Raylib.Rectangle.create 10. 250. 100. 40.
-let on_screen = []
+let stay_rect4 = Raylib.Rectangle.create 10. 200. 100. 40.
+let on_screen = ref []
 let default_x = 10
 let default_y = 100
 let defaul_spacing = 100
+let block_selected = ref false
+let block_selected2 = ref false
 
 open Raylib
 
 let update_x block =
   if
     is_mouse_button_down MouseButton.Left
+    && (not !block_selected)
     && within block
          (float_of_int (get_mouse_x ()))
          (float_of_int (get_mouse_y ()))
-  then get_mouse_x () - 50
-  else int_of_float (Rectangle.x block)
+  then
+    let _ = block_selected := true in
+    get_mouse_x () - 50
+  else
+    let _ = block_selected := false in
+    int_of_float (Rectangle.x block)
 
 let update_y block =
   if
     is_mouse_button_down MouseButton.Left
+    && (not !block_selected2)
     && within block
          (float_of_int (get_mouse_x ()))
          (float_of_int (get_mouse_y ()))
-  then get_mouse_y () - 20
-  else int_of_float (Rectangle.y block)
+  then
+    let _ = block_selected2 := true in
+    get_mouse_y () - 20
+  else
+    let _ = block_selected2 := false in
+    int_of_float (Rectangle.y block)
 
 (** Changes the blocks position if the mouse is clicking on it*)
 let change_block_position block =
@@ -73,6 +87,15 @@ let turn_code_block () =
     (int_of_float (Rectangle.x block +. 10.))
     (int_of_float (Rectangle.y block +. 5.))
     16 Color.black
+
+let move_block color text text_color block =
+  let _ = change_block_position block in
+  let _ = draw_rectangle_rec block color in
+
+  draw_text text
+    (int_of_float (Rectangle.x block +. 10.))
+    (int_of_float (Rectangle.y block +. 5.))
+    16 text_color
 
 let testing_station2 () =
   let block = stay_rect2 in
@@ -108,6 +131,14 @@ let end_button () =
     (int_of_float (Rectangle.y block +. 10.))
     16 Color.black
 
+let create_move_block () =
+  if is_mouse_button_pressed MouseButton.Left && not !block_selected then
+    on_screen := Raylib.Rectangle.create 10. 100. 100. 40. :: !on_screen
+
+let draw_on_screen () =
+  let _ = List.map (move_block Color.gold "Move Cat" Color.black) !on_screen in
+  ()
+
 let rec loop () =
   if window_should_close () then Raylib.close_window
   else
@@ -125,6 +156,12 @@ let rec loop () =
     draw_text "Workspace"
       ((Raylib.get_screen_width () / 4) + 10)
       68 16 Color.black;
+    if
+      within stay_rect2
+        (float_of_int (get_mouse_x ()))
+        (float_of_int (get_mouse_y ()))
+    then create_move_block ();
+    draw_on_screen ();
     start_button ();
     testing_station2 ();
     move_code_block ();
