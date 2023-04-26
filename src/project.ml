8ffb64fab6c2605e6d370920ea6a7b4dd313b97d
Richie Sun <rs929@cornell.edu>
2023-04-26 01:18:39 -0400

    Test

--- src/project.ml
+++ src/project.ml
@@ -145,7 +145,7 @@ let rec loop () =
     let _ = 10 in
     begin_drawing ();
     clear_background Color.raywhite;
-    draw_text "OScratch" 10 10 48 Color.blue;
+    draw_text "OScratch " 10 10 48 Color.blue;
     draw_rectangle 0 60 (Raylib.get_screen_width ()) 3 Color.black;
     draw_rectangle
       (Raylib.get_screen_width () / 4)
